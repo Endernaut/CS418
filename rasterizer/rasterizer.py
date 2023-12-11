2d404d4a477bde13cd731083c1c3321d694b7301
James Huang <jiajunhuang1106@gmail.com>
2023-12-11 04:31:49 -0600

    Working on rasterizer electives

--- rasterizer/rasterizer.py
+++ rasterizer/rasterizer.py
@@ -15,7 +15,6 @@ def dda(a, b, i):
     e = math.ceil(a[i]) - a[i]
     o = e * s
     p = a + o  
-
     return np.array([p, s])
 
 def convert_sRGB(colors):
@@ -26,6 +25,9 @@ def convert_sRGB(colors):
 
 # Flags
 sRGB = 0
+depth = 0
+hyp = 0
+
 
 # png
 text = open(sys.argv[1], "r")
@@ -34,7 +36,11 @@ w = int(l[1])
 h = int(l[2])
 fn = l[3].strip("\n")
 
+# Image
 image = Image.new("RGBA", (w, h), (0,0,0,0))
+
+# Check for depth
+depth_vals = np.ones((w, h))
 
 for i in text:
     l = np.array(i.split())
@@ -47,23 +53,21 @@ for i in text:
     if l[0] == "color":
         c = int(l[1])
         num = (len(l) - 2) // c
+        print(sRGB)
         if c == 3:
             cs = [[float(l[3*i+2]), float(l[3*i+3]), float(l[3*i+4])] for i in range(num)]
-            if sRGB:
-                cs = convert_sRGB(cs)
         else:
+            colors = 4
             cs = [[float(l[4*i+2]), float(l[4*i+3]), float(l[4*i+4]), float(l[4*i+5])] for i in range(num)]
-            if sRGB:
-                cs = convert_sRGB(cs)
 
-    # Position
+    # Position (x, y, z, w)
     if l[0] == "position":
         n = int(l[1])
         num = (len(l) - 2) // n
         if n == 2:
-            pos = [[float(l[2*i+2]), float(l[2*i+3])] for i in range(num)]  
+            pos = [[float(l[2*i+2]), float(l[2*i+3]), 0, 1] for i in range(num)]  
         elif n == 3:
-            pos = [[float(l[3*i+2]), float(l[3*i+3])] for i in range(num)]  
+            pos = [[float(l[3*i+2]), float(l[3*i+3]), float(l[3*i+4]), 1] for i in range(num)]  
         else:
             pos = [[float(l[4*i+2]), float(l[4*i+3]), float(l[4*i+4]), float(l[4*i+5])] for i in range(num)]
 
@@ -71,18 +75,24 @@ for i in text:
     if l[0] == "sRGB":
         sRGB = 1
 
+    # depth
+    if l[0] == "depth":
+        depth = 1
+
+    # hyp
+    if l[0] == "hyp":
+        hyp = 1
+
     # drawArraysTriangles
     if l[0] == "drawArraysTriangles":
         s, count = int(l[1]), int(l[2])
-
+        
         for i in range(count // 3):
             # Get top, middle, bottom y values
             vals = np.array([pos[(i*3) + s],pos[(i*3) + s+1],pos[(i*3) + s+2]])
             cols = np.array([cs[(i*3) + s],cs[(i*3) + s+1],cs[(i*3) + s+2]])
             tot = np.concatenate((vals, cols),axis=1)
             tot = tot[np.argsort(tot[:,1])]
-            print(tot)
-            
             # Divide by w, viewport
             wc = tot[:, 3].copy()
             tot[:,:] /= tot[:, 3, None]
@@ -90,7 +100,7 @@ for i in text:
             tot[:, 0] = (tot[:, 0] + 1) * w / 2
             tot[:, 1] = (tot[:, 1] + 1) * h / 2
             tot[:, 3] = 1 / wc
-            tot[:, 4:] /= tot[:, 3, None]
+            
             
             # Assign top, middle, bottom vectors and setup
             t = tot[0]
@@ -107,12 +117,19 @@ for i in text:
                         if xp1.size != 0:
                             while (xp1[0][0] < th[0][0] or xp1[0][0] < setup[0][0]):
                                 if (xp1[0][0] < w and xp1[0][0] >=0 and xp1[0][1] < h and xp1[0][1] >= 0):
-                                    print("One")
-                                    print(xp1)
-                                    xp1[:,4:] = convert_sRGB(xp1[:,4:])
-                                    print("Two")
-                                    print(xp1)
-                                    image.im.putpixel((int(xp1[0][0]), int(xp1[0][1])), (int(xp1[0][4] * 255), int(xp1[0][5] * 255), int(xp1[0][6] * 255)))
+                                    position = (int(xp1[0][0]), int(xp1[0][1]))
+                                    w_val = xp1[0][3]
+                                    color = [xp1[0][4], xp1[0][5], xp1[0][6]]
+                                    if sRGB:
+                                        color = convert_sRGB(color)
+                                    new_color = (int(color[0] / w val * 255), int(color[1] / w val * 255), int(color[2] / w val * 255))
+                                    if (depth): 
+                                        if (xp1[0][2] < depth_vals[int(xp1[0][0]), int(xp1[0][1])]):
+                                            image.im.putpixel(position, new_color)
+                                            depth_vals[int(xp1[0][0]), int(xp1[0][1])] = xp1[0][2]
+                                    else:
+                                        image.im.putpixel(position, new_color)
+                                    
                                 xp1[0] += xp1[1]
                         
                         th[0] += th[1]
@@ -127,7 +144,20 @@ for i in text:
                         if xp2.size != 0:
                             while (xp2[0][0] < bh[0][0] or xp2[0][0] < setup[0][0]):
                                 if (xp2[0][0] < w and xp2[0][0] >= 0 and xp2[0][1] < h and xp2[0][1] >= 0):
-                                    image.im.putpixel((int(xp2[0][0]), int(xp2[0][1])), (int(xp2[0][4] * 255), int(xp2[0][5] * 255), int(xp2[0][6] * 255)))
+                                    position = (int(xp2[0][0]), int(xp2[0][1]))
+                                    w_val = xp2[0][3]
+                                    color = [xp2[0][4], xp2[0][5], xp2[0][6]]
+                                    if sRGB:
+                                        color = convert_sRGB(color)
+                                    new_color = (int(color[0] / w val * 255), int(color[1] / w val * 255), int(color[2] / w val * 255))
+                                    if (depth): 
+                                        if (xp2[0][2] < depth_vals[int(xp2[0][0]), int(xp2[0][1])]):
+                                            image.im.putpixel(position, new_color)
+                                            depth_vals[int(xp2[0][0]), int(xp2[0][1])] = xp2[0][2]
+                                    else:
+                                        image.im.putpixel(position, new_color)
+                                    
+
                                 xp2[0] += xp2[1]
                         bh[0] += bh[1]
                         setup[0] += setup[1]
@@ -135,7 +165,6 @@ for i in text:
     # Elements
     if l[0] == "elements":
         elems = l[1:].astype(np.uint8)
-        print(elems)
 
     # drawElementsTriangles
     if l[0] == "drawElementsTriangles":
@@ -172,7 +201,17 @@ for i in text:
                         if xp1.size != 0:
                             while (xp1[0][0] < th[0][0] or xp1[0][0] < setup[0][0]):
                                 if (xp1[0][0] < w and xp1[0][0] >=0 and xp1[0][1] < h and xp1[0][1] >= 0):
-                                    image.im.putpixel((int(xp1[0][0]), int(xp1[0][1])), (int(xp1[0][4] * 255), int(xp1[0][5] * 255), int(xp1[0][6] * 255)))
+                                    position = (int(xp1[0][0]), int(xp1[0][1]))
+                                    w_val = xp1[0][3]
+                                    color = (int(xp1[0][4] / w_val * 255), int(xp1[0][5] / w_val * 255), int(xp1[0][6] / w_val * 255))
+                                    if sRGB:
+                                        color = convert_sRGB(color)
+                                    if (depth): 
+                                        if (xp1[0][2] < depth_vals[int(xp1[0][0]), int(xp1[0][1])]):
+                                            image.im.putpixel(position, color)
+                                            depth_vals[int(xp1[0][0]), int(xp1[0][1])] = xp1[0][2]
+                                    else:
+                                        image.im.putpixel(position, color)
                                 xp1[0] += xp1[1]
                         
                         th[0] += th[1]
@@ -187,7 +226,15 @@ for i in text:
                         if xp2.size != 0:
                             while (xp2[0][0] < bh[0][0] or xp2[0][0] < setup[0][0]):
                                 if (xp2[0][0] < w and xp2[0][0] >= 0 and xp2[0][1] < h and xp2[0][1] >= 0):
-                                    image.im.putpixel((int(xp2[0][0]), int(xp2[0][1])), (int(xp2[0][4] * 255), int(xp2[0][5] * 255), int(xp2[0][6] * 255)))
+                                    position = (int(xp2[0][0]), int(xp2[0][1]))
+                                    color = (int(xp2[0][4] / w_val * 255), int(xp2[0][5] / w_val * 255), int(xp2[0][6] / w_val * 255))
+                                    if sRGB:
+                                        color = convert_sRGB(color)
+                                    if (xp2[0][2] < depth_vals[int(xp2[0][0]), int(xp2[0][1])]):
+                                        image.im.putpixel(position, color)
+                                        depth_vals[int(xp2[0][0]), int(xp2[0][1])] = xp2[0][2]
+                                    else:
+                                        image.im.putpixel(position, color)
                                 xp2[0] += xp2[1]
                         bh[0] += bh[1]
                         setup[0] += setup[1]
